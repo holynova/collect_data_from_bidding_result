0cbfb16f47d90b2ddcedb3cd294dcbad9913e0db
moonreader <holy_nova@126.com>
2016-05-17 10:07:41 +0800

    save and load bid data to json

--- scan.py
+++ scan.py
@@ -4,8 +4,22 @@ import xlrd
 import os,datetime
 import json
 
-class Bid:pass
+class Bid:
+	def __init__(self):
+		self.is_blank = True
 class Pack:pass
+
+#写入当前文件夹下以时间命名的函数
+def save_to_file(common_name,content,file_type='.json'):
+	str_now = datetime.datetime.now().strftime('%y%m%d_%H-%M-%S')
+	result_file_name = os.path.dirname(os.path.abspath(__file__)) +'\\'+common_name +str_now+file_type
+
+	with open(result_file_name,"w") as result_file:
+		result_file.write(content.encode('utf-8'))
+
+	print "Saved to file: " + result_file_name
+	return result_file_name
+
 
 class Scanner():
 	def __init__(self,folder):
@@ -22,30 +36,38 @@ class Scanner():
 					except:
 						print 'Error:fail to open ',wb_name
 						continue
-					print wb_name
+					# print wb_name
 					
 					bid = Bid()
 					cnt_bid = 0
 					for sht in wb.sheets():
-						if sht.name.find(u'基本信息')!=-1 or sht.cell_value(0,0) == u"投标信息组":
+						try:
+							sht.name,sht.cell_value(0,0)
+						except:
+							print 'Error: blank worksheet found in ',wb_name,sht.name
+							continue
+
+						if sht.name.find(u'基本信息')!=-1 or ( sht.cell_value(0,0) and sht.cell_value(0,0) == u"投标信息组"):
 							# print '--',sht.name
 							# bid = Bid()
+							bid.is_blank = False
 							cnt_bid += 1
 							if cnt_bid > 1:
 								#出现了第二个基本信息
-								
-							bid.work_num = she.cell_value(1,0)
-							bid.date = she.cell_value(2,0)
-							bid.sr = she.cell_value(3,0)
-							bid.company = she.cell_value(4,0)
-							bid.project = she.cell_value(5,0)
-							bid.csc = she.cell_value(6,0)
-							bid.scoring = she.cell_value(7,0)
+								print 'Warning: find two bids in one file :' ,wb_name
+							# print f,isinstance(f,str),isinstance(f,unicode)
+							# bid.file_name = f
 
+							bid.work_num = sht.cell_value(1,1)
+							bid.date = sht.cell_value(2,1)
+							bid.sr = sht.cell_value(3,1)
+							bid.company = sht.cell_value(4,1)
+							bid.project = sht.cell_value(5,1)
+							bid.csc = sht.cell_value(6,1)
+							bid.scoring = sht.cell_value(7,1)
+							# bid.
 							bid.packs = []
 							# self.bids.append(bid)
-
-
 
 						elif sht.cell_value(0,0) == u"序号" and sht.cell_value(0,1) ==u"厂家":
 							# print '----',sht.name
@@ -64,18 +86,41 @@ class Scanner():
 							bid.packs.append(pack)
 
 
-					self.bids.append(bid)
+					if not bid.is_blank:
+						self.bids.append(bid)
 
-	def save_as_json(self):
-		pass
+	def to_json(self):
+		json_str = 	json.dumps(self.bids,default = lambda o:o.__dict__,indent = 4)
+		return json_str
 
-											
-
-
+					
 
 folder = r"E:\kuaipan\github\collect_data_from_bidding_result\bid_xls"
-folder = r"E:\kuaipan\github\collect_data_from_bidding_result\test"
+# folder = r"E:\kuaipan\github\collect_data_from_bidding_result\test"
 
 
 s = Scanner(folder)
 s.scan()
+file_content = s.to_json()
+save_to_file('bid',file_content,'.json')
+
+
+def json_load(json_file_name):
+	try:
+		f = file(json_file_name,'r')	
+	except:
+		print 'Error:failed to open file ',json_file_name
+	return json.load(f)
+
+
+json_file_name = r'E:\kuaipan\github\collect_data_from_bidding_result\bid.json'
+j = json_load(json_file_name)
+
+print len(j)
+
+output_str = 'date,winner,winner_price,average_no_peak,median,num_company,nkt_price,nkt_gm3,project,company,pack_num\n'
+for bid in j:
+	for pack in bid['packs']:
+		output_str += "%s,%s,%.2f,%.2f,%.2f,%s,%.2f,%.4f,%s,%s,%s\n" %(bid['date'],pack['winner'],pack['winner_price'],pack['average_no_peak'],pack['median'],pack['num_company'],pack['nkt_price'],pack['nkt_gm3'],bid['project'],bid['company'],pack['pack_num'])
+save_to_file('sum',output_str,'.txt')
+
